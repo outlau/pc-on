81e5bdeaac5d0e2248cc60b0cbf44b1203770042
Laurits Aabye <laurits@sharkcell.dk>
2019-04-16 22:16:26 +0200

    remove pug

--- app.js
+++ app.js
@@ -4,34 +4,15 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-//Import the mongoose module
-const mongoose = require('mongoose');
-
 const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
 
 const app = express();
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-// //Set up default mongoose connection
-// const mongoDB = 'mongodb://outlau:tupac21@ds223015.mlab.com:23015/test-db';
-// mongoose.connect(mongoDB);
-// // Get Mongoose to use the global promise library
-// mongoose.Promise = global.Promise;
-// //Get the default connection
-// const db = mongoose.connection;
-//
-// //Bind connection to error event (to get notification of connection errors)
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use('/api', indexRouter);
 // app.use('/users', usersRouter);
